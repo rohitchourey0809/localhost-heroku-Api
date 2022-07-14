846607f59f99da92d76a53588d837ca6d1deea54
ROHIT CHOUREY <97465195+rohitchourey0809@users.noreply.github.com>
2022-07-14 23:40:45 +0530

    first

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const importdata = require("./data.json");
 
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("hello world");
